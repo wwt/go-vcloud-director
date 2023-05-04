215fc09b54356e12580823a239f07670824d7e76
Alexander A Anselmo <Alex.Anselmo@wwt.com>
2023-05-04 17:30:38 -0500

    Add Router Info

--- types/v56/types.go
+++ types/v56/types.go
@@ -236,11 +236,10 @@ type NetworkConfiguration struct {
 	// When they are both nil, it means the "internal" interface (the default) will be used.
 	// When one of them is set, the corresponding interface will be used.
 	// They cannot be both set (we'll get an API error if we do).
-	SubInterface         *bool `xml:"SubInterface,omitempty"`
-	DistributedInterface *bool `xml:"DistributedInterface,omitempty"`
-	GuestVlanAllowed     *bool `xml:"GuestVlanAllowed,omitempty"`
-	// TODO: Not Implemented
-	RouterInfo RouterInfo `xml:"RouterInfo,omitempty"`
+	SubInterface         *bool      `xml:"SubInterface,omitempty"`
+	DistributedInterface *bool      `xml:"DistributedInterface,omitempty"`
+	GuestVlanAllowed     *bool      `xml:"GuestVlanAllowed,omitempty"`
+	RouterInfo           RouterInfo `xml:"RouterInfo,omitempty"`
 	// SyslogServerSettings           SyslogServerSettings `xml:"SyslogServerSettings,omitempty"`
 }
 
